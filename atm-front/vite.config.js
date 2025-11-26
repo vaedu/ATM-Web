8a09877183a085d2c7163b1bafd35f31443cf1ab
vaedu <vaedu070418@gmail.com>
2025-11-26 21:43:34 +0800

    1

--- atm-front/vite.config.js
+++ atm-front/vite.config.js
@@ -4,11 +4,13 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src'),
-            include: ['crypto-js']
+            '@': path.resolve(__dirname, './src')
         }
+    },
+    optimizeDeps: {
+        include: ['crypto-js']  // 预优化 crypto-js，避免 Rollup 解析失败
     }
 })
